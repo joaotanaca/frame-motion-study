96519410f58736ff192aa142cb64a0a303644463
Tanaca <joao.ramos@evtit.com>
2021-06-17 22:34:27 -0300

    finish animations

--- src/components/FirstAndSecond/index.tsx
+++ src/components/FirstAndSecond/index.tsx
@@ -14,9 +14,54 @@ function FirstAndSecond() {
         [0.198, 0.264, 0.558, 0.627],
         [1, 0.511, 0.511, 1],
     );
+    const firstRadius = useTransform(
+        scrollYProgress,
+        [0.198, 0.264, 0.558, 0.627],
+        [0, 4, 4, 0],
+    );
+    const leftHeightSide = useTransform(
+        scrollYProgress,
+        [0, 0.558],
+        ["20vh", "100vh"],
+    );
+    const rightSideScale = useTransform(
+        scrollYProgress,
+        [0.047, 0.93],
+        [0, 0.511],
+    );
+    const rightSideY = useTransform(
+        scrollYProgress,
+        [0.047, 0.93],
+        ["58vh", "0vh"],
+    );
+    const offsetY = useTransform(
+        scrollYProgress,
+        [0.328, 0.397, 0.461, 0.53],
+        ["0%", "-100%", "-100%", "-200%"],
+    );
     return (
         <Sticky className="second">
-            <Sticky className="first" style={{ scale: firstScale }} />
+            <Sticky
+                className="first"
+                style={{ scale: firstScale, borderRadius: firstRadius }}
+            >
+                <motion.div className="offset" style={{ y: offsetY }}>
+                    <div className="a">
+                        <motion.div
+                            className="left-side"
+                            style={{ height: leftHeightSide }}
+                        />
+                        <div className="right-side">
+                            <motion.div
+                                className="right-image"
+                                style={{ y: rightSideY, scale: rightSideScale }}
+                            />
+                        </div>
+                    </div>
+                    <div className="b"></div>
+                    <div className="c"></div>
+                </motion.div>
+            </Sticky>
             <motion.div
                 style={{
                     position: "absolute",
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 export const Main = styled.main`
     height: 1610vh;
+    padding: 0 30px;
 `;
 
 export const Section = styled(motion.section)`
@@ -10,15 +11,38 @@ export const Section = styled(motion.section)`
         background-color: red;
         overflow: hidden;
         .offset {
+            width: 100%;
+            height: 100%;
         }
         .a {
             background: #f5f1ea;
+            height: 100%;
+            display: flex;
+            align-items: flex-end;
+            .left-side {
+                width: 50%;
+                height: 100%;
+                background-color: #7dbb91;
+            }
+            .right-side {
+                width: 50%;
+                height: 100%;
+                .right-image {
+                    width: 100%;
+                    height: 100%;
+                    background-color: #7dbb91;
+                }
+            }
         }
         .b {
             background: #582534;
+            width: 100%;
+            height: 100%;
         }
         .c {
             background: #0a7397;
+            width: 100%;
+            height: 100%;
         }
     }
     .second {
